733a92a182f4b8db6759f64c42ee8305c6222f31
iliahrom <iliahrom@gmail.com>
2024-11-30 17:22:42 +0200

    Update Task3.js

--- Task3/Task3.js
+++ Task3/Task3.js
@@ -11,10 +11,13 @@ class 48/6
 
 /*
 Write a program that defines an array and counts the amount of zero's.
-*use conditional expression, if statmant not allowed.
+*use conditional expression, if statement  not allowed.
 */
 const arr = [123, 100, 0, 1234, 645, 2131];
 let counter = 0;
+
+//runs over each element of the array and checks if it equals by value to zero.
 arr.forEach((num) => (counter += num === 0 ? 1 : 0));
 
 console.log(counter);
+
